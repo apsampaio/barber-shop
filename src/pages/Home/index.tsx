4b5d40941dbd5fcbcfb18b46ecada296f0cd16d4
apsampaio <andre03.sampaio@gmail.com>
2023-05-03 14:00:48 -0300

    :bulb: chore: add section 3 and four

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import "./styles.css";
 
+import PomadaImage from "../../assets/chief-blue-web.jpg";
+
 const Home: React.FC = () => {
   return (
     <div className="wrapper">
@@ -18,23 +20,102 @@ const Home: React.FC = () => {
       </section>
       <section className="two">
         <div className="content">
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque cum
-            dicta, ut debitis, incidunt laborum quibusdam sint quis modi quos
-            corrupti recusandae nemo. Consectetur labore vitae quidem corporis
-            unde aut.
-          </p>
+          <span className="section-title">
+            <span></span>
+            <h1>NOSSOS SERVIÇOS</h1>
+            <span></span>
+          </span>
+          <div className="services">
+            <div>
+              <img src="/cut.svg" alt="corte" />
+              <h4>Corte social</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adi</p>
+            </div>
+            <div>
+              <img src="/beard.svg" alt="barba" />
+              <h4>Barba</h4>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Facilis sit aperiam voluptates vel cupiditate porro repudiandae
+              </p>
+            </div>
+            <div>
+              <img src="/painting.svg" alt="tintura" />
+              <h4>Tintura de cabelo</h4>
+              <p>Lorem ipsum dolor sit amet Facilis sit aperiam voluptate</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <h2></h2>
+              <p></p>
+            </div>
+          </div>
         </div>
       </section>
       <section className="three">
         <div className="content">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-            aliquam consectetur quibusdam, magnam quisquam vitae? Excepturi iste
-            voluptatem provident dignissimos nostrum laudantium dolorem.
-            Accusamus animi consequatur debitis blanditiis laboriosam
-            reiciendis!
-          </p>
+          <span className="section-title">
+            <span></span>
+            <h1>PORTFOLIO</h1>
+            <span></span>
+          </span>
+          <div className="portfolio">
+            <div className="column">
+              <img src="/portfolio/pic1.jfif" alt="pic1" />
+              <img src="/portfolio/pic2.jfif" alt="pic2" />
+              <img src="/portfolio/pic5.jfif" alt="pic5" />
+            </div>
+            <div className="column">
+              <img src="/portfolio/pic3.jfif" alt="pic3" />
+              <img src="/portfolio/pic4.jfif" alt="pic4" />
+            </div>
+          </div>
+          <button>
+            <p>VER MAIS</p>
+          </button>
+        </div>
+      </section>
+      <section className="four">
+        <div className="content">
+          <span className="section-title">
+            <span></span>
+            <h1>PRODUTOS</h1>
+            <span></span>
+          </span>
+          <div className="products">
+            <div className="product">
+              <img src={PomadaImage} alt="pomada" />
+              <h4>Chief Pomade</h4>
+              <p>À base de água</p>
+            </div>
+            <div className="product">
+              <img src={PomadaImage} alt="pomada" />
+              <h4>Chief Pomade</h4>
+              <p>À base de água</p>
+            </div>
+            <div className="product">
+              <img src={PomadaImage} alt="pomada" />
+              <h4>Chief Pomade</h4>
+              <p>À base de água</p>
+            </div>
+            <div className="product">
+              <img src={PomadaImage} alt="pomada" />
+              <h4>Chief Pomade</h4>
+              <p>À base de água</p>
+            </div>
+          </div>
+          <button>
+            <p>ENCOMENDAR</p>
+          </button>
+        </div>
+      </section>
+      <section className="five">
+        <div className="content">
+          <span className="section-title">
+            <span></span>
+            <h1>PRODUTOS</h1>
+            <span></span>
+          </span>
         </div>
       </section>
     </div>
